1b73cbc12114705f40c52655b7f255cefdf974c6
Shenghao Dong <shenghaodong18@gmail.com>
2022-09-06 21:50:21 -0400

    valid.cpp completed. Also added other cpp files

---
+++ valid.cpp
@@ -0,0 +1,24 @@
+/*
+Author: your name
+Course: CSCI-133
+Instructor: Mike Zamansky
+Assignment: LAB2A
+
+Here, briefly, at least in one or a few sentences
+describe what the program does.
+*/
+
+#include <iostream>
+#include <cmath>
+
+int main(){
+    int num;
+    std::cout << "Please enter an integer: ";
+    std::cin >> num;
+    while(num < 0 || num >= 100){
+        std::cout << "Please re-enter: ";
+        std::cin >> num;
+    }
+    std::cout << "Number squared is " << num*2 << std::endl;
+    return 0;
+}
